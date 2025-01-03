d18c8ee3d827f0ada07bc87f348b3f26fdae51be
banchich <banchich@amazon.com>
2025-01-03 09:08:11 -0500

    test

--- target/debug/deps/foo-8e8ae0e678dc90b6.d
+++
@@ -1,5 +0,0 @@
-/home/ANT.AMAZON.COM/banchich/Documents/semver-test/target/debug/deps/libfoo-8e8ae0e678dc90b6.rmeta: foo/src/main.rs
-
-/home/ANT.AMAZON.COM/banchich/Documents/semver-test/target/debug/deps/foo-8e8ae0e678dc90b6.d: foo/src/main.rs
-
-foo/src/main.rs:
